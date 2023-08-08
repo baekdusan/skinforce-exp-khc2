d8326e2bac3331b1e573fa68e7051a724c70c338
baekdusan <santoo@unist.ac.kr>
2023-08-08 17:24:32 +0900

    전반적인 기능을 모두 구현하였음. 디테일 및 센서 이식 필요.

--- pseudoskin.py
+++ pseudoskin.py
@@ -27,18 +27,18 @@ class ExperimentApp:
                            tk.PhotoImage(file="back.png"),
                            tk.PhotoImage(file="forearm.png")]
         self.idx = 0
+        self.scale = 0
 
         ##############################
 
-
         # 화면 전환을 위한 프레임들
         self.frames = []
-        for _ in range(4):
+        for _ in range(23):
             frame = tk.Frame(self.root)
             self.frames.append(frame)
         
         # 현재 화면 인덱스
-        self.current_frame_index = 0
+        self.current_frame_index = 0    
         
         # 프레임들에 내용 추가
         self.add_content_to_frames()
@@ -50,6 +50,28 @@ class ExperimentApp:
         self.setSensor_thread.daemon = True  # 메인 스레드 종료 시 함께 종료되도록 설정
         self.setSensor_thread.start()
         
+    def add_content_to_frames(self):
+        self.functionList = []
+        page = 0
+        self.functionList.append((self.initialPage, (page,)))  # initialPage 함수와 파라미터 저장
+        page += 1
+        targets = ["너클", "손등", "전완"]
+        for i in range(3):
+            self.functionList.append((self.calibrationPage, (targets[i], i, page)))  # calibrationPage 함수와 파라미터 저장
+            page += 1
+            for j in range(12, 21, 4):
+                self.functionList.append((self.preExperimentPage, (page,)))  # preExperimentPage 함수와 파라미터 저장
+                page += 1
+                self.functionList.append((self.experimentPage, (targets[i], j, page)))  # experimentPage 함수와 파라미터 저장
+                page += 1
+        self.functionList.append((self.endPage, (page,)))
+
+    # 필요한 시점에 함수 호출
+    def execute_function(self, index):
+        if 0 <= index < len(self.functionList):
+            func, params = self.functionList[index]
+            func(*params)
+
     def setSensor(self):
         connected_ports = []
         for i in sp.comports():
@@ -68,16 +90,11 @@ class ExperimentApp:
                     line = line.decode().strip()
                     try:
                         self.value = float(line)
-                        self.moveSensorLine()
+                        if hasattr(self, 'stickCanvas'):
+                            self.moveSensorLine(self.scale)
                     except ValueError:
                         self.value = 0
-
-    def add_content_to_frames(self):
-        self.initialPage()
-        self.calibrationPage()
-        self.preExperimentPage()
-        self.experimentPage()
-
+  
     # 피험자 번호 중복 체크 후 button active
     def check_duplicate(self, num, button):
         if num not in self.numList:
@@ -94,9 +111,9 @@ class ExperimentApp:
         return
 
     # 실험 시작 화면
-    def initialPage(self):
+    def initialPage(self, page):
 
-        frame = self.frames[0]
+        frame = self.frames[page]
 
         titlefont= tkinter.font.Font(size=96, weight= "bold")
         buttonfont = tkinter.font.Font(size=72, weight= "normal")
@@ -131,8 +148,8 @@ class ExperimentApp:
         ##########
 
     # 측정 화면
-    def calibrationPage(self):
-        frame = self.frames[1]
+    def calibrationPage(self, target, image, page):
+        frame = self.frames[page]
 
         titlefont= tkinter.font.Font(size= 96, weight= "bold")
         explainfont = tkinter.font.Font(size= 72, weight= "bold")
@@ -151,7 +168,7 @@ class ExperimentApp:
         middle = tk.Frame(frame)
         middle.pack(fill= "both", expand= True)
 
-        label=tkinter.Label(middle, image=self.handImages[0])
+        label=tkinter.Label(middle, image=self.handImages[image])
         label.pack(pady= 20)
 
         ###########
@@ -165,15 +182,13 @@ class ExperimentApp:
         next_button = tk.Button(bottom, text="다음", state= "disable", command=self.show_next_frame, font= buttonfont)
         next_button.pack(side= "right", padx= 100)
 
-        explain_label = tk.Label(bottom, text= "너클 부분을 측정합니다.\n 측정 버튼을 눌러주세요.", font= explainfont)
+        explain_label = tk.Label(bottom, text= target + " 부분을 측정합니다.\n 측정 버튼을 눌러주세요.", font= explainfont)
         explain_label.pack(anchor= "center")
 
         ##########
 
-        frame.bind("<return>",)
-
-    def preExperimentPage(self):
-        frame = self.frames[2]
+    def preExperimentPage(self, page):
+        frame = self.frames[page]
 
         titlefont= tkinter.font.Font(size=96, weight= "bold")
         buttonfont = tkinter.font.Font(size=72, weight= "normal")
@@ -190,11 +205,13 @@ class ExperimentApp:
         startButton = tk.Button(bottom, text= "실험 시작", font= buttonfont, command= self.show_next_frame)
         startButton.pack(pady= 120)
 
-    def experimentPage(self):
-        frame = self.frames[3]
+    def experimentPage(self, target, scale, page):
+        self.scale = scale
+        frame = self.frames[page]
+
         partFont = tkinter.font.Font(size= 72, weight = "bold")
         progressFont = tkinter.font.Font(size= 32, weight= "normal")
-        whichPart = tk.Label(frame, text= "Knuckle", font= partFont)
+        whichPart = tk.Label(frame, text= target, font= partFont)
         
         ##########
         
@@ -207,59 +224,82 @@ class ExperimentApp:
 
         stick = self.stickCanvas.create_rectangle(0, 0, self.width / 10, self.height * 0.9, fill= "white smoke")
 
-        self.now = self.stickCanvas.create_rectangle(0, self.height * 0.9, self.width / 10, self.height * 0.9 / 20 * 21, fill= "light pink")
+        self.now = self.stickCanvas.create_rectangle(0, self.height * 0.9, self.width / 10, self.height * 0.9 / scale * (scale + 1), fill= "light pink")
 
         x1, y1, x2, y2 = self.stickCanvas.coords(stick)
 
-        self.gapY = (y2 - y1) / 20
-        for i in range(20):
+        self.gapY = (y2 - y1) / scale
+
+        for i in range(scale):
             y = y1 + (i * self.gapY)
             self.stickCanvas.create_line(x1, y, x2, y, fill= "light grey")
         ##########
-        progressLabel = tk.Label(frame, text= "1/20", font= progressFont)
-        progressLabel.place(x = self.width - 100, y = self.height - 300, anchor= "se")
+        self.progressLabel = tk.Label(frame, text= str(self.idx) + " / " + str(self.scale), font= progressFont)
+        self.progressLabel.place(x = self.width - 100, y = self.height - 300, anchor= "se")
         ##########
         self.sensorBox = self.stickCanvas.create_rectangle(x1, y2, x2, y2 + self.gapY, fill= "pale violet red")
-        self.sensorLine = self.stickCanvas.create_line(x1, y2, x2, y2, width= self.gapY / 20)
+        self.sensorLine = self.stickCanvas.create_line(x1, y2, x2, y2, width= self.gapY / scale)
         
         ##########
-        self.nextButton = tk.Button(frame, text= "다음", command= self.show_problems, font= partFont)
+        self.nextButton = tk.Button(frame, text= "다음", command= lambda: self.show_problems(scale), font= partFont)
         self.nextButton.place(x = self.width - 100, y = self.height - 100, anchor= "se")
         ##########
-        self.problems = [y1 + i * self.gapY for i in range(20)]
+        self.problems = [y1 + i * self.gapY for i in range(scale)]
         random.shuffle(self.problems)
+    
+    def endPage(self, page):
+        frame = self.frames[page]
+
+        titlefont= tkinter.font.Font(size=96, weight= "bold")
+        buttonfont = tkinter.font.Font(size=72, weight= "normal")
+        
+        top = tk.Frame(frame)
+        top.pack(side= "top", fill= "both", expand= True)
+
+        label = tk.Label(top, text= "실험이 모두 끝났습니다. 감사합니다.", font= titlefont)
+        label.pack(anchor= "s", expand= True)
+
+        bottom = tk.Frame(frame)
+        bottom.pack(side= "bottom", fill= "both", expand= True)
+
+        endButton = tk.Button(bottom, text= "종료", font= buttonfont, command= self.root.destroy)
+        endButton.pack(pady= 120)
   
-    def moveSensorLine(self):
+    def moveSensorLine(self, scale):      
         stickHeight = self.height * 0.9
         fraction = min(self.value / 1500, 1)
         linePlace = stickHeight - stickHeight * fraction
         x1, _, x2, _ = self.stickCanvas.coords(self.sensorLine)
 
-        for i in range(1, 21):
+        for i in range(1, scale + 1):
             if (i - 1) * self.gapY <= linePlace < i * self.gapY:
                 self.stickCanvas.coords(self.sensorBox, x1, (i - 1) * self.gapY, x2, i * self.gapY)
                 break
 
         self.stickCanvas.coords(self.sensorLine, x1, linePlace, x2, linePlace)
    
-    def show_problems(self):
+    def show_problems(self, scale):
+        
+        if self.idx == scale:
+            self.idx = 0
+            self.show_next_frame()
+            return
+
         # 현재 now 사각형의 좌표를 가져옴
         x1, _, x2, _ = self.stickCanvas.coords(self.now) 
 
         newY = self.problems[self.idx]
         self.stickCanvas.coords(self.now, x1, newY, x2, newY + self.gapY)
-        self.idx += 1
 
-        if self.idx == 20:
-            self.nextButton.config(state= "disable")
+        self.idx += 1
+        self.progressLabel.config(text= str(self.idx) + " / " + str(self.scale))
 
     def show_frame(self):
-        # 현재 화면 표시
+        self.execute_function(self.current_frame_index)
         frame = self.frames[self.current_frame_index]
         frame.pack(fill=tk.BOTH, expand=True)
 
     def show_next_frame(self):
-        # 다음 화면 표시
         self.frames[self.current_frame_index].pack_forget()
         self.current_frame_index = (self.current_frame_index + 1) % len(self.frames)
         self.show_frame()
